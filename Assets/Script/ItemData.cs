5f9d11572ffd07b09d08ce85904958ac7cf2b4ff
bogay <pojay11523@gmail.com>
2021-08-17 01:45:00 +0800

    Import item data

---
+++ Assets/Script/ItemData.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GoGooseGo
+{
+    [CreateAssetMenu(menuName = "Item")]
+    public class ItemData : ScriptableObject
+    {
+        [field : SerializeField]
+        public int id { get; private set; }
+
+        [field : SerializeField]
+        public new string name { get; private set; }
+
+        [field : SerializeField]
+        public string description { get; private set; }
+
+        [field : SerializeField]
+        public Sprite sprite { get; private set; }
+    }
+}
