9ac42d2400544ca0ba7c9bbe8bf29f002cd85464
bogay <pojay11523@gmail.com>
2021-08-17 12:41:41 +0800

    Inject ItemCollection

--- Assets/Script/StageInstaller.cs
+++ Assets/Script/StageInstaller.cs
@@ -7,12 +7,16 @@ namespace GoGooseGo
     public class StageInstaller : ScriptableObjectInstaller<StageInstaller>
     {
         public PlayerData playerData;
+        public ItemCollection defaultItems;
+
         public override void InstallBindings()
         {
             this.playerData.Init();
             Container.BindInstance(this.playerData).AsSingle();
             var player = FindObjectOfType<PlayerControl>();
             Container.BindInstance(player).AsSingle();
+            // Clone a deafult item collection
+            Container.BindInstance(GameObject.Instantiate(this.defaultItems));
         }
     }
 }
